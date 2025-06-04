702ddea39d806946453fff273e72f4fe13eb7870
Stanley Mwinde <salvadorethorbide8@gmail.com>
2025-06-04 12:35:11 +0300

    why this is for you is done, ill be back to make it responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Currency } from "@/components/common";
-import { Account, Investing } from "@/components/home";
+import { Account, Investing, WhyforYou } from "@/components/home";
 import SwiperPage from "@/components/swiper";
 import { Heading } from "@chakra-ui/react";
 
@@ -10,6 +10,7 @@ export default function Home() {
       <Currency />
       <Account />
       <Investing />
+      <WhyforYou />
     </>
   );
 }
--- components/home/index.ts
+++ components/home/index.ts
@@ -1,2 +1,3 @@
 export { default as Account } from "./Account";
 export { default as Investing } from "./Investing";
+export { default as WhyforYou } from "./WhyforYou";
