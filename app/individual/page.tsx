009577744bdad3c59bd562c78600ba1af9defbcb
Stanley Mwinde <salvadorethorbide8@gmail.com>
2025-06-05 11:20:36 +0300

    invest with purpose

--- app/individual/page.tsx
+++ app/individual/page.tsx
@@ -1,4 +1,5 @@
 import {
+  Connect,
   ConsultationTeam,
   FinancialGoals,
   Hero,
@@ -15,6 +16,7 @@ const page = () => {
       <FinancialGoals />
       <InvestmentSolutions />
       <ConsultationTeam />
+      <Connect />
     </>
   );
 };
--- components/individual/index.ts
+++ components/individual/index.ts
@@ -3,3 +3,4 @@ export { default as Rates } from "./Rates";
 export { default as FinancialGoals } from "./FinancialGoals";
 export { default as InvestmentSolutions } from "./InvestmentSolutions";
 export { default as ConsultationTeam } from "./ConsultationTeam";
+export { default as Connect } from "./Connect";
