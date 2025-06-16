5ef1b74c752e9cc31e9652a00d9d914aa54befd7
Stanley Mwinde <salvadorethorbide8@gmail.com>
2025-06-16 16:44:04 +0300

    fixed slider links and texts

--- components/swiper/index.tsx
+++ components/swiper/index.tsx
@@ -10,6 +10,9 @@ import "swiper/css/pagination";
 
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import { Box, Button, Text } from "@chakra-ui/react";
+import { link } from "fs";
+import Link from "next/link";
+import { text } from "stream/consumers";
 
 interface AutoplayTimeLeftParams {
   swiper: import("swiper").Swiper;
@@ -57,7 +60,7 @@ const SwiperPage = () => {
         <SwiperSlide key={index}>
           <Box
             position="relative"
-            height="85vh"
+            height="90vh"
             width="100%"
             backgroundImage={`url(${image.image})`}
             backgroundSize="cover"
@@ -67,7 +70,7 @@ const SwiperPage = () => {
             justifyContent="flex-start"
             px={{ base: 4, md: 20 }}
           >
-            <Box maxW="lg" color="white">
+            <Box color="#0a2234">
               <Text
                 fontSize={{ base: "3xl", md: "5xl" }}
                 fontWeight="bold"
@@ -78,21 +81,23 @@ const SwiperPage = () => {
                 you love
               </Text>
               <Text fontSize={{ base: "md", md: "xl" }} mt={6}>
-                Start Your Freedom Plan
+                {image.text}
               </Text>
-              <Button
-                mt={6}
-                colorScheme="blackAlpha"
-                bg="blackAlpha.800"
-                color="white"
-                px={8}
-                py={6}
-                rounded="full"
-                fontWeight="bold"
-                _hover={{ bg: "blackAlpha.700" }}
-              >
-                Start Investing
-              </Button>
+              <Link href={image.link} passHref>
+                <Button
+                  mt={6}
+                  colorScheme="blackAlpha"
+                  bg="blackAlpha.800"
+                  color="white"
+                  px={8}
+                  py={6}
+                  rounded="full"
+                  fontWeight="bold"
+                  _hover={{ bg: "blackAlpha.700" }}
+                >
+                  Start Investing
+                </Button>
+              </Link>
             </Box>
           </Box>
         </SwiperSlide>
@@ -115,20 +120,28 @@ const slider_images = [
     image: "/images/slide-1.png",
     title: "Make Money While doing what you love",
     description: "Start Your Freedom Plan",
+    text: "Start Your Freedom Plan",
+    link: "/",
   },
   {
     image: "/images/slide-2.png",
     title: "Make Money While doing what you love",
     description: "Explore What's Possible",
+    text: "Explore What's Possible",
+    link: "/",
   },
   {
-    image: "/images/slide-3.png",
+    image: "/images/slider-3.png",
     title: "Make Money While doing what you love",
     description: "Go Global with Nabo",
+    text: "Go Global with Nabo",
+    link: "/global-investing",
   },
   {
     image: "/images/slide-4.png",
     title: "Make Money While doing what you love",
     description: "Clock Out, We've got you",
+    text: "Clock Out, We've got you",
+    link: "/",
   },
 ];
