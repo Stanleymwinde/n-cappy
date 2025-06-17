d3bb9ca59d78830a5f4f51d14301cc6b3afb2773
Stanley Mwinde <salvadorethorbide8@gmail.com>
2025-06-17 13:35:26 +0300

    finished toolas and resources page

--- app/tools_and_resources/page.tsx
+++ app/tools_and_resources/page.tsx
@@ -1,6 +1,7 @@
 import { Blog } from "@/components/blogs";
 import { Currency } from "@/components/common";
 import Hero from "@/components/common/Hero";
+import { CallToAction } from "@/components/home";
 import { Calculator, MoreResources, ResourcesTabs } from "@/components/tools";
 import { Heading } from "@chakra-ui/react";
 import React from "react";
@@ -19,6 +20,7 @@ const page = () => {
       <Blog />
       <MoreResources />
       <ResourcesTabs />
+      <CallToAction />
     </>
   );
 };
