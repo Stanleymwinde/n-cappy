c0d9e5d5f83057806cb9a9c3230f5fc99d3bc32e
Stanley Mwinde <salvadorethorbide8@gmail.com>
2025-07-30 12:09:54 +0300

    fixed currency to rates

--- app/individual/MMF&FIF/page.tsx
+++ app/individual/MMF&FIF/page.tsx
@@ -1,4 +1,4 @@
-import { Currency } from "@/components/common";
+import { Rates } from "@/components/common";
 import Hero from "@/components/common/Hero";
 import { CallToAction } from "@/components/home";
 import { CompareFunds, FIF, MMF } from "@/components/individual";
@@ -7,13 +7,14 @@ import React from "react";
 const page = () => {
   return (
     <>
+      <Rates />
       <Hero
         title="Grow Your Wealth With Nabo Capital"
         imageUrl="/images/MMF.png"
         subtitle="Choose the investment fund that aligns with your financial goals."
         button="Explore Funds"
       />
-      <Currency />
+
       <MMF />
       <FIF />
       <CompareFunds />
