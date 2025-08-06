4236d39e229e3d7d8c850888fe794aa1c4f0b9ee
CarolineGatwiri-12 <cgatwiri@dimetechgroup.com>
2025-08-06 14:13:21 +0300

    added the missing sections on institutional page

--- components/institutional/index.ts
+++ components/institutional/index.ts
@@ -3,4 +3,9 @@ export { default as Team } from "./team";
 export { default as PartnerWithUs } from "./partner-with-us";
 export { default as WhatAreYourGoals } from "./WhatAreYourGoals";
 export { default as TreasuryGoals } from "./TreasuryGoals";
+export { default as WhoDoWeServe } from "./WhoDoWeServe";
+export { default as OurInvestmentSolutions } from "./OurInvestmentSolutions";
+export { default as WhyOrgsChooseUs } from "./WhyOrgsChooseUs";
 export { default as InvestmentSolutions } from "./InvestmentSolutions";
+
+
