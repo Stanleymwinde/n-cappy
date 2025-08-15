e013185443de4a5cbe693c7814e5ad0b0b837cc1
CarolineGatwiri-12 <cgatwiri@dimetechgroup.com>
2025-08-16 00:03:43 +0300

    Hid the seconds counter

--- components/swiper/index.tsx
+++ components/swiper/index.tsx
@@ -110,11 +110,11 @@ const SwiperPage = () => {
         </SwiperSlide>
       ))}
 
-      <div className="autoplay-progress" slot="container-end">
+      <div className="autoplay-progress" slot="container-end"  style={{ display: "none" }}>
         <svg viewBox="0 0 48 48" ref={progressCircle}>
           <circle cx="24" cy="24" r="20"></circle>
         </svg>
-        <span ref={progressContent}></span>
+        <span ref={progressContent}  style={{ display: "none" }}></span>
       </div>
     </Swiper>
   );
