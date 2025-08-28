7ef869804d6f6e0f8757b993ffa323085394b0e0
CarolineGatwiri-12 <cgatwiri@dimetechgroup.com>
2025-08-28 11:31:08 +0300

    Added the right images on lifestyle goal hero

--- app/lifestyle-goal/page.tsx
+++ app/lifestyle-goal/page.tsx
@@ -11,15 +11,15 @@ const page = () => {
       <Hero
         button="Unlock My Plan"
         button1="Learn More"
-        imageUrl="/images/lifestyle-hero.png"
+        imageUrl="/images/life-style.jpeg"
         title="Live Purposefully.Save Deliberately."
         subtitle="Secure the day-to-day, and create room to breathe, plan, and enjoy life."
       />
       <PlanForJoy
         title="Do you want steady monthly returns that cover life's core expenses?"
         subtitle="With this plan, we help you replace your monthly income so that even if you lose your job, take a break, or face unexpected changes, your life keeps moving forward."
-        subtext="You’ll gain breathing room—the kind that covers your basic needs without stress. Whether it’s keeping the lights on or paying school fees, your essentials stay protected. Always."
-        imageUrl="/images/lifestyle.png"
+        subtext="You will gain breathing room; the kind that covers your basic needs without stress. Whether it’s keeping the lights on or paying school fees, your essentials stay protected. Always."
+        imageUrl="/images/heroafrica.jpeg"
       />
       <QuestionPacks />
       <CallToAction />
